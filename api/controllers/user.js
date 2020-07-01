5d97b4795f915dce3ac56aab45263961d49b66e9
Ben Hong <ben@bencodezen.io>
2020-07-01 16:53:00 -0400

    feature: add basic user fetching api

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,16 +1,6 @@
-'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
-
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
-
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
-var util = require('util');
+'use strict'
+var util = require('util')
+var userDB = require('../../data/users.json')
 
 /*
  Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
@@ -25,8 +15,8 @@ var util = require('util');
   we specify that in the exports of this module that 'hello' maps to the function named 'hello'
  */
 module.exports = {
-  hello: hello
-};
+  user: user
+}
 
 /*
   Functions in a127 controllers used for operations should take two parameters:
@@ -34,11 +24,15 @@ module.exports = {
   Param 1: a handle to the request object
   Param 2: a handle to the response object
  */
-function hello(req, res) {
+function user(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
+  var name = req.swagger.params.username.value || 'stranger'
+  var user = userDB[name]
 
   // this sends back a JSON response which is a single string
-  res.json(hello);
+  if (name) {
+    return res.json(user)
+  } else {
+    return res.json({ status: 500, message: 'No user found' })
+  }
 }
