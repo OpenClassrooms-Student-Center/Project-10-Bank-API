436ee130e81e55d774a3c670646bbd3b98b2c2c2
Ben Hong <ben@bencodezen.io>
2020-07-04 12:44:15 -0400

    feature: add middleware for payload and cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const dotEnv = require('dotenv')
+const cors = require('cors')
 
 dotEnv.config()
 
 const app = express()
+
+// Handle CORS issues
+app.use(cors())
+
+// Request payload middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3001
 
