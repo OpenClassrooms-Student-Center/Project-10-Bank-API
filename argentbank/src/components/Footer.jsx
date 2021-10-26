0891152afdeda4170dea52934eb7fa96731db93a
Amandine Arias <amandine.arias@gmail.com>
2021-10-26 21:43:38 +0200

    feat (component) : Footer

---
+++ argentbank/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+}
