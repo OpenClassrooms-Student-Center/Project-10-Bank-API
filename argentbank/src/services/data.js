023775aa3e566d18c98b2ca10afd52110c1e0a8b
Amandine Arias <amandine.arias@gmail.com>
2021-10-26 21:44:57 +0200

    feat (services) : home page data

---
+++ argentbank/src/services/data.js
@@ -0,0 +1,31 @@
+import chat from '../img/icon-chat.png';
+import money from '../img/icon-money.png';
+import security from '../img/icon-security.png';
+
+export const homeData = [
+    {
+      id: '1',
+      icon: chat,
+      alt: 'chat icon',
+      title: 'You are our #1 priority',
+      paragraph:
+        'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    },
+    {
+      id: '2',
+      icon: money,
+      alt: 'money icon',
+      title: 'More savings means higher rates',
+      paragraph:
+        'The more you save with us, the higher your interest rate will be!',
+    },
+    {
+      id: '3',
+      icon: security,
+      alt: 'security icon',
+      title: 'Security you can trust',
+      paragraph:
+        'We use top of the line encryption to make sure your data and money is always safe.',
+    },
+  ];
+
