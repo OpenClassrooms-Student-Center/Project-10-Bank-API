a53fc78ee905ca0c747bf8f35ec873fb158f8417
Amandine Arias <amandine.arias@gmail.com>
2021-10-26 21:45:40 +0200

    feat (page component) : Home page

---
+++ argentbank/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Feature from '../components/Feature';
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Feature />
+    </main>
+  );
+}
