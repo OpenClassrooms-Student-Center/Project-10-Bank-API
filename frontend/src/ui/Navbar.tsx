73614a3006a221115e9647d927125670363aad3d
corentin <corentinfarcy1@gmail.com>
2023-06-21 14:32:37 +0200

    feat(AB3): create logout

--- frontend/src/ui/Navbar.tsx
+++ frontend/src/ui/Navbar.tsx
@@ -1,7 +1,18 @@
 import { NavLink } from 'react-router-dom'
 import Logo from '../assets/img/argentBankLogo.png'
+import { useDispatch, useSelector } from 'react-redux'
+import { getToken } from '../auth/authSelectors.ts'
+import { logout } from '../auth/authActions.ts'
+import { ThunkDispatch } from 'redux-thunk'
 
 export const Navbar = () => {
+  const token = useSelector(getToken)
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>()
+
+  const handleLogout = () => {
+    dispatch(logout())
+  }
+
   return (
     <nav className="main-nav">
       <NavLink className="main-nav-logo" to="/">
@@ -13,10 +24,17 @@ export const Navbar = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
       <div>
-        <NavLink className="main-nav-item" to="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </NavLink>
+        {token ? (
+          <NavLink to="/" className="main-nav-item" onClick={handleLogout}>
+            <i className="fa fa-user-circle"></i>
+            Sign Out
+          </NavLink>
+        ) : (
+          <NavLink className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </NavLink>
+        )}
       </div>
     </nav>
   )
