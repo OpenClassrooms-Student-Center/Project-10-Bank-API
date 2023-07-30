ece4f9d21f2844e3147c868374b8756f02e46723
cyrian simard <cyriansimard@gmail.com>
2023-07-30 13:35:21 +0200

    fix db connection with dotEnv

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('./swagger.yaml')
-const dbConnection = require('./database/connection')
 
 dotEnv.config()
 
@@ -12,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 // Connect to the database
+const dbConnection = require('./database/connection')
 dbConnection()
 
 // Handle CORS issues
