f60bf094875ee7234a0b05061f8274441354b7fa
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-07-31 15:06:12 +0200

    added components

--- argent-bank-app/src/pages/Profile/Profile.jsx
+++ argent-bank-app/src/pages/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import './profile.css'
 import Button from '../../components/Button/Button'
+import Account from '../../components/Account/Account'
 
 function Profile() {
     return (
@@ -9,6 +10,21 @@ function Profile() {
                 <Button text="Edit Name" className="edit-button" />
             </div>
             <h2 className="sr-only">Accounts</h2>
+            <Account 
+                title="Argent Bank Checking (x8349)" 
+                amount="$2,082.79"
+                description="Available Balance"
+            />
+            <Account 
+                title="Argent Bank Savings (x6712)" 
+                amount="$10,928.42"
+                description="Available Balance"
+            />
+            <Account 
+                title="Argent Bank Credit Card (x8349)" 
+                amount="$184.30"
+                description="Current Balance"
+            />
         </main>
     )
 }
