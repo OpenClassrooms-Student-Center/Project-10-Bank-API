2c6c9a56acc9c1fb4577155695a6acc93acd61b8
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-07-31 11:26:44 +0200

    added navigation

---
+++ argent-bank-app/src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from '../layout/Header/Header';
+import Footer from '../layout/Footer/Footer'
+import Home from '../pages/Home/index'
+import Login from '../pages/Login/Login';
+import Profile from '../pages/Profile/Profile';
+import Error from '../pages/Error/Error'
+
+
+function Navigation() {
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='*' element={<Error />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
+}
+
+export default Navigation;
--- argent-bank-app/src/index.js
+++ argent-bank-app/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/Home';
 
+import Navigation from './components/Navigation';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Navigation />
   </React.StrictMode>
 );
 
