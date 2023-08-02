3c1aa33633e03ae68d78d1813f8e35b876fc823c
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-08-02 14:35:39 +0200

    created URL const

---
+++ argent-bank-app/src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'http://localhost:3001/api/v1/user'
+
+export const URL_LOGIN = API_URL + '/login'
+export const URL_SIGNUP = API_URL + '/signup'
+export const URL_PROFILE = API_URL + '/profile'
