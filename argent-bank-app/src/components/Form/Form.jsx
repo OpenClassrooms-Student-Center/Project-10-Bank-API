fed7b803bfff818c9b0e6a4586d1b35037633a16
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-08-02 14:31:10 +0200

    fixed database access

--- argent-bank-app/src/components/Form/Form.jsx
+++ argent-bank-app/src/components/Form/Form.jsx
@@ -10,22 +10,22 @@ function Form() {
             <h1>Sign In</h1>
             <form>
                 <div className="input-wrapper">
-                    <label for="username">Username</label
+                    <label htmlFor="username">Username</label
                     ><input type="text" id="username" />
                 </div>
                 <div className="input-wrapper">
-                    <label for="password">Password</label
+                    <label htmlFor="password">Password</label
                     ><input type="password" id="password" />
                 </div>
                 <div className="input-remember">
-                    <input type="checkbox" id="remember-me" /><label for="remember-me"
+                    <input type="checkbox" id="remember-me" /><label htmlFor="remember-me"
                     >Remember me</label
                     >
                 </div>
                 {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
                 <Link to="/profile" className="sign-in-button">Sign In</Link>
                 {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
-                {/* <button class="sign-in-button">Sign In</button> */}
+                {/* <button className="sign-in-button">Sign In</button> */}
 
             </form>
         </section>
